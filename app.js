ecdcd1f376186facbfb784a606e879ea7e264349
Swat I <swat.iyengar@gmail.com>
2021-07-29 19:35:11 -0400

    d3 update

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ Promise.all([
     const dataRx = data[0]
     const dataIsland = data[1]
 
-
     // .map = goes through every data point and says, pull out your drug - list comprehension...maps every row of old data set and puts it into the new dataset; then new set creates a new uniquelist from an existing list
 
     let options = new Set(dataRx.map(d => d.drug))
+
 
 
     //d3 selects our element with drug id
@@ -44,7 +44,7 @@ Promise.all([
         //give it an on click event
         .on('click', function() {
             //Selects island_intro and the blurb and hides
-            d3.selectAll('.island-intro, .blurb, .affordability').classed('hide', false)
+            d3.selectAll('.island-intro').classed('hide', false)
                 //save drug name for next d3 calculation
             drug = d3.select('.drug').node().value
             console.log(drug)
@@ -59,14 +59,11 @@ Promise.all([
                 }
             })
 
-            // const formatMoney = function(d) { return "$" + formatDecimalComma(d); }
-
+            // format annual-cost
             d3.select('.selection-annualcost').text(ac).text(function() { return formatMoney(ac); })
-
-
-
             console.log(ac)
-                //Filter island data sold at a price less than annualcost variable
+
+            //Filter island data sold at a price less than annualcost variable
             let filtered = dataIsland.filter(function(d) {
                 if (+d['price'] <= ac) {
                     return d
@@ -78,70 +75,32 @@ Promise.all([
             let randomIsland = filtered[Math.floor(Math.random() * filtered.length)];
             console.log(randomIsland)
 
-            // d3 group the information about these islands where:
-            // let islandGroup = {
-            //     islandName: randomIsland['name'],
-            //     islandRegion: randomIsland['region'],
-            //     islandPhoto: randomIsland['image'], //browser should load photo
-            //     islandAcreage: randomIsland['acreage'],
-            //     islandTag: randomIsland['tags']
-            // }
+            function isRandomIsland() {
+                islandName = randomIsland['name'],
+                    islandPrice = randomIsland['price'],
+                    islandRegion = randomIsland['region'],
+                    islandPhoto = randomIsland['image'],
+                    islandAcreage = randomIsland['acreage'],
+                    islandTag = randomIsland['tags']
 
-            // d3 group the information about these islands where:
-            islandName = randomIsland['name'],
-                islandRegion = randomIsland['region'],
-                islandPhoto = randomIsland['image'], //browser should load photo
-                islandAcreage = randomIsland['acreage'],
-                islandTag = randomIsland['tags']
+                d3.select('.islandName').text(islandName)
+                d3.select('.islandPrice').text(islandPrice).text(function() { return formatMoney(islandPrice); })
+                d3.select('.islandRegion').text(islandRegion)
+                d3.select("img").attr("src", islandPhoto)
+                d3.select('.islandAcreage').text(islandAcreage + " acre(s)")
+                d3.select('.islandTags').text("Tags: " + islandTag)
+            }
+            isRandomIsland();
 
-            console.log(randomIsland['name'])
+            d3.select('.affordability-selected-drug').text(drug)
 
-            // console.log(islandGroup)
+            let annualIncome = document.getElementById('userAnnualIncome').value
+            console.log(annualIncome)
 
-            //Group of nominations to which different elements are added
-            const group = d3.select('.nominations-sentence')
+            let nonadjusted_yrsworked = (ac / annualIncome)
+            console.log(nonadjusted_yrsworked)
 
+            d3.select('.nonadjusted_yrsworked').text(function() { return formatDecimal(nonadjusted_yrsworked) + " years"; })
 
-            //Selects nomination class from HTML
-            //     d3.select('.nominations-sentence')
-            //     .selectAll('.nomination')
-            //Removes existing elemnts at each click
-            //     .remove()
-
-            d3.select('.islands')
-                //Selects nomination class from HTML again (grab everything that is the class of nomination) - every span gets a row of the data
-
-            //Tells d3 what data is about to be bound
-            .data(randomIsland)
-                .enter()
-                .append("text")
-                .attr('class')
-                .text(randomIsland['name'])
-                //Creates the selection - are some of the data points lonely? only affects the new
-                //    .enter()
-                //Updates the selection such that the correct data elements appear - if lonely, add a span
-                //    .append('span')
-                //    .join removes the need for enter/append unless transitions are involved
-                // .join('class')
-                // //  gives the data a class
-                // .attr('class', 'islandName')
-                // //  tells the page to the display an element of the data
-                // .text(d => d.randomIsland);
-
-
-            // //  Select the element with the class movie
-            // d3.select('.movie')
-            //     //  And set the text as the name of the first element
-            //     .text(winner[0].Name);
         })
-
-
-
-    // d3 select <div class=islands> and append the island group information there
-
-    // save the user submitted value of the income slider
-
-    // d3 take the annualcost of the selected drug and divide it by the user's annual income - save this value to nonadjusted_yrwork
-
-    // append this data to <span class="nonadjusted_yrsworked"></span></p>
 })
